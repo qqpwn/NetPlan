f0390ccbaf2aecbe9452c7517ab50aa17f8efbe4
Patrick Poul Nielsen <patr9244@edu.easj.dk>
2019-11-22 11:38:06 +0100

    lavet mapper og "start klasser til jan"

---
+++ NetPlan/Model/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetPlan.Model
+{
+    public class Users
+    {
+        private int _id;
+        private string _name;
+        private int _phone;
+        private string _email;
+        private string _password;
+        private string _description;
+        private int _accessLevel;
+
+
+
+
+
+    }
+}
