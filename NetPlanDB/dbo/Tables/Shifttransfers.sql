eec012b5fb20258e8b41ac735e61625b88f4a18c
Patrick Poul Nielsen <patr9244@edu.easj.dk>
2019-11-26 12:32:13 +0100

    Database tilføjet

---
+++ NetPlanDB/dbo/Tables/Shifttransfers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Shifttransfers] (
+    [id]                 INT     NOT NULL,
+    [fk_shiftRequest]    INT     NULL,
+    [fk_shifts]          INT     NULL,
+    [fk_users_primary]   INT     NULL,
+    [fk_users_secondary] INT     NULL,
+    [approved]           TINYINT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ NetPlanDB/dbo/Tables/corrections.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[corrections] (
+    [id]                  INT     NOT NULL,
+    [fk_users]            INT     NULL,
+    [fk_shifts_primary]   INT     NULL,
+    [fk_shifts_secondary] INT     NULL,
+    [hours]               REAL    NULL,
+    [approved]            TINYINT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
