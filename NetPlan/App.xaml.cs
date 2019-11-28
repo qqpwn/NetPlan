003df5026952b878851f74bc6b3c50502b29c989
Patrick Poul Nielsen <patr9244@edu.easj.dk>
2019-11-28 09:25:40 +0100

    reverse eng. på uwp

--- NetPlan/App.xaml.cs
+++ NetPlan/App.xaml.cs
@@ -66,7 +66,7 @@ namespace NetPlan
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage1), e.Arguments);
+                    rootFrame.Navigate(typeof(Page2), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
