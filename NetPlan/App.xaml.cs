545a0773d8f4e9628ba90e972b9cd4c923ac608b
Larsvegasmoo <larsvegasbad@hotmail.com>
2019-11-29 14:27:51 +0100

    asd

--- NetPlan/App.xaml.cs
+++ NetPlan/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Netplan.View;
 
 namespace NetPlan
 {
@@ -66,7 +67,7 @@ namespace NetPlan
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(Page2), e.Arguments);
+                    rootFrame.Navigate(typeof(Logind), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
--- NetPlan/View/Logind.xaml.cs
+++ NetPlan/View/Logind.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Netplan;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,7 +16,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace NetPlan.View
+namespace NetPlan
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
